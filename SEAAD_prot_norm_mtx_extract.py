d439d39f665ff4f6bf4df91b22302ab81788a13f
shannjiang <31867080+shannjiang@users.noreply.github.com>
2023-10-03 14:27:12 -0400

    Add files via upload

--- SEAAD_prot_norm_mtx_extract.py
+++ SEAAD_prot_norm_mtx_extract.py
@@ -14,16 +14,17 @@ from scipy.sparse import csr_matrix
 # In[9]:
 
 
-work_dir = '/home/shann/Documents/cziscience/SEAAD/'
-out_dir = work_dir
-gene_id_df = pd.read_csv('/home/shann/Documents/Natura/proteosome/gene_id.txt', sep = '\t', header = 0)
+work_dir = '/home/sj3195/cziscience/SEAAD/'
+out_dir = work_dir + 'proteasome/'
+gene_id_df = pd.read_csv('/home/sj3195/proteasome/gene_id.txt', sep = '\t', header = 0)
 prefix = 'prot'
 
 
 # In[3]:
 
 
-celltypes = ['L23_IT','Astrocyte','Chandelier','Endothelial','L4_IT','L5_ET','L5_IT','L56_NP','L6b','L6_CT','L6_IT_Car3','L6_IT','Lamp5','Lamp5_Lhx6','Microglia_PVM','Oligo','OPC','Pax6','Pvalb','Sncg','Sst_Chodl','Sst','Vip','VLMC']
+celltypes = ['L23_IT','Astrocyte','Chandelier','Endothelial','L4_IT','L5_ET','L5_IT','L56_NP','L6b','L6_CT','L6_IT_Car3','L6_IT','Lamp5','Lamp5_Lhx6','Microglia_PVM','Oligodendrocyte','OPC','Pax6','Pvalb','Sncg','Sst_Chodl','Sst','Vip','VLMC']
+broadClass = ['Glutamatergic','Astrocyte','GABAergic','Endothelial','Glutamatergic','Glutamatergic','Glutamatergic','Glutamatergic','Glutamatergic','Glutamatergic','Glutamatergic','Glutamatergic','GABAergic','GABAergic','Microglia','Oligodendrocyte','OPC','GABAergic','GABAergic','GABAergic','GABAergic','GABAergic','GABAergic','VLMC']
 brainRegions = ['MTG','DLPFC']
 
 
